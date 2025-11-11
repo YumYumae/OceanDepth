773d8153f20232538595a8b405f419c7d6d840ca
chaymaetaj407-glitch <chaymaetaj407@gmail.com>
2025-11-11 13:04:53 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,19 +6,16 @@
 #include "../include/creatures.h"
 
 int main() {
-    srand(time(NULL)); // Pour générer des attaques aléatoires
+    srand(time(NULL));
 
-    // --- Initialisation du joueur ---
     Plongeur joueur;
     initialiserJoueur(&joueur);
     afficherStatutJoueur(joueur);
 
-    // --- Création d'une créature de test ---
     CreatureMarine requin = {1, "Requin-Tigre", 90, 90, 15, 25, 5, 3, "aucun", 1};
 
     printf("\n🌊 Une créature apparaît : %s !\n", requin.nom);
 
-    // --- Lancer le combat ---
     demarrerCombat(&joueur, &requin);
 
     printf("\n🏁 Fin du test de combat.\n");
